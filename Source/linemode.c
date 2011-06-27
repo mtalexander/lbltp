8b6f0b7a6b88335e75548b17296fd3e7003d0fd5
mta <mta@4486c24c-adcd-11db-87d7-73a06ae0c2b1>
2011-06-27 04:40:03 +0000

    Update version number to 1.3.
    git-svn-id: https://svn.msalexander.com:3218/repo/utilities/lbltp/trunk@58 4486c24c-adcd-11db-87d7-73a06ae0c2b1

--- Source/linemode.c
+++ Source/linemode.c
@@ -42,7 +42,7 @@ static int rewcmd(int parsc,unsigned char *parsv[]);
 static char UNUSED(copywrite[640])  = "Copyright (c) 1992-1997 Regents of the University of Michigan. All rights reserved. Redistribution and use in source and binary forms are permitted provided that this notice is preserved and that due credit is given to the University of Michigan at Ann Arbor. The name of the University may not be used to endorse or promote products derived from this software without specific prior written permission. This software is provided ``as is'' without express or implied warranty.";
 static void unvattn(int);
 static char var_name[256];
-static char version[10]="1.2";
+static char version[10]="1.3";
 
 
 
